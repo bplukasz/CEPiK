cf86485eab2e2c26ae1916c05c6fe7da0c5581b3
bplukasz <bp.lukasz@gmail.com>
2017-11-10 23:50:25 +0100

    Initial Commit

---
+++ Data/DbInitializer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CEPiK.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(CepikContext context)
+        {
+            context.Database.EnsureCreated();
+        }
+    }
+}
