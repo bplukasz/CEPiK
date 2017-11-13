b147f153ef06ad026ea2dae507e776c1be2ff655
Grzesiek <pg2464@o2.pl>
2017-11-13 11:58:12 +0100

    Updatewith Cepikcontex modification

--- Data/CepikContext.cs
+++ Data/CepikContext.cs
@@ -1,11 +1,37 @@
-﻿using System;
+﻿using CEPiK.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace CEPiK.Data
 {
-    public class CepikContext
+    public class CepikContext : DbContext
     {
+        public CepikContext(DbContextOptions<CepikContext> options) : base(options)
+        {
+        }
+
+
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Carjacking> Carjackings { get; set; }
+        public DbSet<Deregister> Deregisters { get; set; }
+        public DbSet<EditHistory> EditHistory { get; set; }
+        public DbSet<RegistrationBook> RegistrationBooks { get; set; }
+        public DbSet<TemporaryLicense> TemporaryLicenses { get; set; }
+        public DbSet<Editor> Editors { get; set; }
+        public DbSet<Fuel> Fuels { get; set; }
+        public DbSet<TechnicalResearch> TechnicalResearchs { get; set; }
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<TidalWithdrawal> TidalWithdrawals { get; set; }
+        public DbSet<LegalizationMark> LegalizationMarks { get; set; }
+        public DbSet<Insurance> Insurances { get; set; }
+        public DbSet<VehicleCard> VehicleCards { get; set; }
+        public DbSet<Loss> Loses { get; set; }
+        public DbSet<TemporaryStickers> TemporaryStickers { get; set; }
+        public DbSet<RegistrationHistory> RegistrationHistory { get; set; }
+        public DbSet<Vehicle> Vehicles { get; set; }
+        public DbContextOptions<CepikContext> Options { get; }
     }
 }
--- Data/DbInitializer.cs
+++ Data/DbInitializer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CEPiK.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,6 +11,12 @@ namespace CEPiK.Data
         public static void Initialize(CepikContext context)
         {
             context.Database.EnsureCreated();
+
+            if (context.Cars.Any())
+            {
+                return;   // DB has been seeded
+            }
+
         }
     }
 }
